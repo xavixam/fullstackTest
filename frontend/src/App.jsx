24f1d194207a16b647fac239c9269dc8842a994a
xavi <antunez.xavi.grao@gmail.com>
2025-09-05 20:47:51 +0200

    component form basic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Form from "./components/Form/Form"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Form/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
