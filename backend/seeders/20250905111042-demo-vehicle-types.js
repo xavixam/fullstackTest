07dba01c9ebb4d321060f178187f053769a3b44a
xavi <antunez.xavi.grao@gmail.com>
2025-09-05 13:32:55 +0200

    seeders completed

--- backend/seeders/20250905111042-demo-vehicle-types.js
+++ backend/seeders/20250905111042-demo-vehicle-types.js
@@ -2,24 +2,14 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('VehicleTypes', [
+      { name: 'Hatchback', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Suv', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Sedan', createdAt: new Date(), updatedAt: new Date() }
+    ]);
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('VehicleTypes', null, {});
   }
 };
--- backend/seeders/20250905111043-demo-vehicles.js
+++ backend/seeders/20250905111043-demo-vehicles.js
@@ -2,24 +2,14 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Vehicles', [
+      { name: 'Seat Ibiza', vehicleTypeId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'Ford Focus', vehicleTypeId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { name: 'BMW 3 Series', vehicleTypeId: 3, createdAt: new Date(), updatedAt: new Date() }
+    ]);
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Vehicles', null, {});
   }
 };
